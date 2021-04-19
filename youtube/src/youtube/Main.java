f397f9749c79e0a5d32357ed2e6b3769728c9dfc
niraj-kr <niraj825316@gmail.com>
2021-04-19 21:30:57 +0530

    first submit

---
+++ youtube/src/youtube/Main.java
@@ -0,0 +1,14 @@
+package youtube;
+
+public class Main {
+
+	final synchronized public static void main(String... args) {
+		System.out.println("main method !");
+		
+
+	}
+	public static int main(int x) {
+		System.out.println("This is second main mehtod !");
+	}
+
+}
